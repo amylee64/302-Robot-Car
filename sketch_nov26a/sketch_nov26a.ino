2c2ff2f51662821de08f592ac5aa88475e1e7beb
will-rogers <43078448+will-rogers@users.noreply.github.com>
2018-11-26 17:41:23 -0600

    a

--- sketch_nov26a/sketch_nov26a.ino
+++ sketch_nov26a/sketch_nov26a.ino
@@ -9,6 +9,7 @@ int led1 = 2;
 int led2 = 3;
 int led3 = 4;
 int sum = 0;
+int sensorValue = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -20,25 +21,27 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  photo1 = analogRead(photoresist1);
-  photo2 = analogRead(photoresist2);
-  photo3 = analogRead(photoresist3);
+  photo1 = (analogRead(photoresist1) - 61)/4;
+  photo2 = (analogRead(photoresist2) - 61)/4;
+  photo3 = (analogRead(photoresist3) - 61)/4;
   sum = photo1 + photo2 + photo3;
   
   // turn the ledPin on
-  digitalWrite(led1, HIGH);
-  delayMicroseconds(photo1);
   digitalWrite(led1, LOW);
+  delayMicroseconds(photo1);
+  digitalWrite(led1, HIGH);
   
-  digitalWrite(led2, HIGH);
-  delayMicroseconds(photo2);
   digitalWrite(led2, LOW);
+  delayMicroseconds(photo2);
+  digitalWrite(led2, HIGH);
 
-  digitalWrite(led3, HIGH);
-  delayMicroseconds(photo3);
   digitalWrite(led3, LOW);
+  delayMicroseconds(photo3);
+  digitalWrite(led3, HIGH);
 
-  Serial.print(photo1);
-  Serial.print("");
+  sensorValue = analogRead(photoresist1);
+ 
+  Serial.println(sensorValue);
+  Serial.print(" ");
 
 }
