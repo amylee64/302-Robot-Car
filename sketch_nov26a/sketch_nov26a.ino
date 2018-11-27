faac4c170e36f3ab9d9f6e79282371ca5e15db51
will-rogers <43078448+will-rogers@users.noreply.github.com>
2018-11-26 18:04:28 -0600

    now with PWM

--- sketch_nov26a/sketch_nov26a.ino
+++ sketch_nov26a/sketch_nov26a.ino
@@ -9,7 +9,6 @@ int led1 = 2;
 int led2 = 3;
 int led3 = 4;
 int sum = 0;
-int sensorValue = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -21,27 +20,18 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  photo1 = (analogRead(photoresist1) - 61)/4;
-  photo2 = (analogRead(photoresist2) - 61)/4;
-  photo3 = (analogRead(photoresist3) - 61)/4;
-  sum = photo1 + photo2 + photo3;
-  
-  // turn the ledPin on
-  digitalWrite(led1, LOW);
-  delayMicroseconds(photo1);
-  digitalWrite(led1, HIGH);
-  
-  digitalWrite(led2, LOW);
-  delayMicroseconds(photo2);
-  digitalWrite(led2, HIGH);
+  photo1 = analogRead(photoresist1) - 55;
+  photo2 = analogRead(photoresist2) - 55;
+  photo3 = analogRead(photoresist3) - 55;
 
-  digitalWrite(led3, LOW);
-  delayMicroseconds(photo3);
-  digitalWrite(led3, HIGH);
-
+  
   sensorValue = analogRead(photoresist1);
  
   Serial.println(sensorValue);
   Serial.print(" ");
+  analogWrite(led1, photo1);
+  analogWrite(led2, photo2);
+  analogWrite(led3, photo3);
 
+  
 }
