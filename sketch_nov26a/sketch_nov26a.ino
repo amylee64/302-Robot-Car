e0f761cde4d9f5cb2c8583485a54322eeeebe980
will-rogers <43078448+will-rogers@users.noreply.github.com>
2018-11-27 16:08:46 -0600

    Update sketch_nov26a/sketch_nov26a.ino

--- sketch_nov26a/sketch_nov26a.ino
+++ sketch_nov26a/sketch_nov26a.ino
@@ -8,7 +8,7 @@ int photo3 = 0;
 int led1 = 9;
 int led2 = 10;
 int led3 = 11;
-int off = 20;
+int off = 30;
 float photo1avg = 0;
 float photo2avg = 0;
 float photo3avg = 0;
@@ -27,22 +27,22 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-   if (analogRead(photoresist1) >= off)
-        photo1 = analogRead(photoresist1) - off;
+   if (analogRead(photoresist1) < off)
+        photo1 = analogRead(photoresist1);
 
-    else if (analogRead(photoresist1) < off)
+    else if (analogRead(photoresist1) > off)
         photo1 = 0;
   
-    if (analogRead(photoresist2) >= off)
-        photo2 = analogRead(photoresist2) - off;
+    if (analogRead(photoresist2) < off)
+        photo2 = analogRead(photoresist2);
 
-    else if (analogRead(photoresist2) < off)
+    else if (analogRead(photoresist2) > off)
         photo2 = 0;
         
-    if (analogRead(photoresist3) >= off)
-        photo3 = analogRead(photoresist3) - off;
+    if (analogRead(photoresist3) < off)
+        photo3 = analogRead(photoresist3);
 
-    else if (analogRead(photoresist3) < off)
+    else if (analogRead(photoresist3) > off)
         photo3 = 0;
 
 /*
@@ -55,7 +55,7 @@ void loop() {
   photo2avg = (photo2 + 3 * photo2avg)/4;
   photo3avg = (photo3 + 3 * photo3avg)/4;
 
-   Serial.println(analogRead(photoresist3));
+   Serial.println(photo3);
   Serial.print(" ");
  
   analogWrite(led1, photo1avg);
