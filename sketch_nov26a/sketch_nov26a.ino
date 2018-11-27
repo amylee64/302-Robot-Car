1c43d1e8d8ffac1fab03587dfc4280a9e5192761
will-rogers <43078448+will-rogers@users.noreply.github.com>
2018-11-27 16:25:36 -0600

    PID input

--- sketch_nov26a/sketch_nov26a.ino
+++ sketch_nov26a/sketch_nov26a.ino
@@ -8,11 +8,12 @@ int photo3 = 0;
 int led1 = 9;
 int led2 = 10;
 int led3 = 11;
-int off = 30;
+int off = 40;
 float photo1avg = 0;
 float photo2avg = 0;
 float photo3avg = 0;
 float off2 = 20;
+float center = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -28,22 +29,26 @@ void setup() {
 void loop() {
   // read the value from the sensor:
    if (analogRead(photoresist1) < off)
-        photo1 = analogRead(photoresist1);
+        photo1 = (-1) * analogRead(photoresist1) + off;
 
     else if (analogRead(photoresist1) > off)
         photo1 = 0;
   
     if (analogRead(photoresist2) < off)
-        photo2 = analogRead(photoresist2);
+        photo2 = (-1) * analogRead(photoresist2) + off;
 
     else if (analogRead(photoresist2) > off)
         photo2 = 0;
         
     if (analogRead(photoresist3) < off)
-        photo3 = analogRead(photoresist3);
+        photo3 = (-1) * analogRead(photoresist3) + off;
 
     else if (analogRead(photoresist3) > off)
         photo3 = 0;
+
+        photo1 = photo1 * 10;
+        photo2 = photo2 * 10;
+        photo3 = photo3 * 10;
 
 /*
         
@@ -55,12 +60,16 @@ void loop() {
   photo2avg = (photo2 + 3 * photo2avg)/4;
   photo3avg = (photo3 + 3 * photo3avg)/4;
 
-   Serial.println(photo3);
-  Serial.print(" ");
- 
   analogWrite(led1, photo1avg);
   analogWrite(led2, photo2avg);
   analogWrite(led3, photo3avg);
+
+  center = (photo1avg * (-1))/(photo1avg + photo2avg + photo3avg) + (photo3avg)/(photo1avg + photo2avg + photo3avg);
+
+  
+  Serial.println(center);
+  Serial.print(" ");
+ 
 
   
 }
