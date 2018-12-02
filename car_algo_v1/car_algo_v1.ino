11b0beb79b78b95000582698a97668d3e744d9b1
Amy Lee <amy.lee@utexas.edu>
2018-12-01 22:57:18 -0600

    Added pin, variable configurations and basic car mvmt
    Added pin configurations
    Added variable initializations
    Added basic car mvmt (just to move the car forwards)

--- car_algo_v1/car_algo_v1.ino
+++ car_algo_v1/car_algo_v1.ino
@@ -1,3 +1,18 @@
+// Pin Configurations *** CHANGE IF NEEDED ***
+const int button = 13;                // pin 13 = switch
+const int motorL1out = 11;           // pin 11 = left motor (lead 1)
+const int motorL2out = 10;           // pin 10 = left motor (lead 2)
+const int motorR1out = 9;            // pin 9 = right motor (lead 1)
+const int motorR2out = 8;            // pin 8 = right motor (lead 2)
+
+const int p1 = A0;      // setting each photoresistor and distance sensor to its own 
+const int p2 = A1;      // analog input pin on the Arduino 
+const int p3 = A2;      // *** WE CAN CHANGE THIS IF NEEDED ***
+const int d1 = A3;
+const int d2 = A4;
+const int d3 = A5;
+
+// Variable Declarations
 int photoR1 = 0;                  // initialize photoresistor values to 0
 int photoR2 = 0;                  // use analogRead in main code
 int photoR3 = 0;
@@ -8,17 +23,16 @@ int distSen3 = 0;
 
 int leftMotor = 0;                // initialize motor speed for left and right
 int rightMotor = 0;               // write values to these variables to control the car motors
-                                 
+
+int buttonState = 0;              // holds state of the switch (on = 1, off = 0)
 // add more variables as needed
-// need to figure out supplying power to car through Arduino
+// need to figure out supplying power to car through Arduino/battery/whatever
 
 void setup() {
-  // put your setup code here, to run once:
-  int buttonPressed = 0;
-  
-  pinMode(switch, INPUT);         // sets "pin" to input (use if switch included)
-  pinMode(pin, OUTPUT);           // "pin" for left motor
-  pinMode(pin, OUTPUT);           // "pin" for right motor  
+  pinMode(motorL1out, OUTPUT);           // left motor lead 1 = output
+  pinMode(motorL2out, OUTPUT);           // left motor lead 2 = output
+  pinMode(motorR1out, OUTPUT);           // right motor lead 1 = output
+  pinMode(motorR2out, OUTPUT);           // right motor lead 2 = output
 
   // insert more pin initializations as needed
   // somehow set "5V" pins to "HIGH" so Arduino powers car? 
@@ -26,38 +40,33 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(button);    // read whether switch on or off 
 
-  // CHECK IF SWITCH IS ON // 
-  buttonPressed = digitalRead(switch);
-  if (buttonPressed == 1) {          // if power on, car moves (if switch included)     
-
-   leftMotor = 10;   // just move car forward
-   rightMotor = 10;
-
-   // CODE TO READ IN SENSOR VALUES GOES HERE
-
-
-
-   // CODE TO CHANGE MOTOR SPEEDS, ETC. GOES HERE
-
-
-
-
-
-
-   // write any speed values to these two variables to change car mvmt
-   // change if needed
-   analogWrite(pin, leftMotor);
-   analogWrite(pin, right Motor); 
-
-    
-    
-
-    
-    
-
-    
+  if (buttonState == HIGH) {
+  /* P1: car stays in the middle of the black tape:
+   *    - Check for black tape (photoresistor), then check for obstacle (distance sensor), repeat
+   *    - Black = certain value from photoresistor reading, check for this continuously
+   *    - when car starts to deviate from path (or if path changes, readjust motor configurations to keep the car on tape
+   *    - once obstacle detected, break from checking the black tape (no longer needed)
+   *    
+   * P2: car navigates around the obstacle:
+   *    - find minimum distance car should be from the obstacle (distance sensor)
+   *    - go around the thing 
+   *    
+   * P3: car navigates through walls and looks for red tape:
+   *    - use distance sensors/flex sensor? to navigate car through the wall portion.
+   *    - keep checking for red tape at end of distance sensor loop portion 
+   *    - once red tape detected, car stops. 
+   */   
+   // when switch on, car will move forwards
+   digitalWrite(motorL1out, HIGH);
+   digitalWrite(motorR1out, HIGH); 
+   
+  } else {
+    digitalWrite(motorL1out, LOW);
+    digitalWrite(motorL2out, LOW);
+    digitalWrite(motorR1out, LOW);
+    digitalWrite(motorR2out, LOW);
   }
 
 }
