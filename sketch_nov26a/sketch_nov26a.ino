d6d45ccfee2fd9c0aa3b581c7b542169f3b3f5f6
Will Rogers <william.rogers@utexas.edu>
2018-12-03 17:45:18 -0600

    Update sketch_nov26a/sketch_nov26a.ino

--- sketch_nov26a/sketch_nov26a.ino
+++ sketch_nov26a/sketch_nov26a.ino
@@ -8,7 +8,9 @@ int photo3 = 0;
 int led1 = 9;
 int led2 = 10;
 int led3 = 11;
-int off = 50;
+int off1 = 420;
+int off2 = 407;
+int off3 = 376;
 float photo1avg = 0;
 float photo2avg = 0;
 float photo3avg = 0;
@@ -18,6 +20,16 @@ float PID = 0;
 int P = 1;
 int I = -1;
 int D = -.1;
+int vel = 100;
+int radius = 100;
+float w = 0;
+float centeravg = 0;
+int velocity = 100;
+int runtime = 100;
+float Y = 0;
+//int posThresh = 0.2;
+//int negThresh = -0.2;
+
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(led1, OUTPUT);
@@ -31,22 +43,22 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-   if (analogRead(photoresist1) < off)
-        photo1 = (-1) * analogRead(photoresist1) + off;
+   if (analogRead(photoresist1) < off1)
+        photo1 = (-1) * analogRead(photoresist1) + off1;
 
-    else if (analogRead(photoresist1) > off)
+    else if (analogRead(photoresist1) > off1)
         photo1 = 0;
   
-    if (analogRead(photoresist2) < off)
-        photo2 = (-1) * analogRead(photoresist2) + off;
+    if (analogRead(photoresist2) < off2)
+        photo2 = (-1) * analogRead(photoresist2) + off2;
 
-    else if (analogRead(photoresist2) > off)
+    else if (analogRead(photoresist2) > off2)
         photo2 = 0;
         
-    if (analogRead(photoresist3) < off)
-        photo3 = (-1) * analogRead(photoresist3) + off;
+    if (analogRead(photoresist3) < off3)
+        photo3 = (-1) * analogRead(photoresist3) + off3;
 
-    else if (analogRead(photoresist3) > off)
+    else if (analogRead(photoresist3) > off3)
         photo3 = 0;
 
         photo1 = photo1 * 10;
@@ -67,31 +79,32 @@ void loop() {
   analogWrite(led2, photo2avg);
   analogWrite(led3, photo3avg);
 
-  centerold = center;
+  //centerold = center;
 
   center = (photo1avg * (-1))/(photo1avg + photo2avg + photo3avg) + (photo3avg)/(photo1avg + photo2avg + photo3avg);
 
+  centeravg = (centerold * 7) + center;
   
   Serial.println(center);
   Serial.print(" ");
 
   delay(4);
 
-  PID = P*center + I*(center+centerold) + D*(center-centerold);
-
   //turning algo
+  
+  //Y = radius - centeravg;
 
-  //Y = radius - center;
+  //radius = (sqrt(2*(velocity * runtime)^2 + Y^2)-Y)/2;
 
-  //radius = (sqrt(2*(velocity*runtime)^2+Y^2)-Y)/2
+  // v^2/r= (w*r)^2/r, w*r=v
 
-  //leftvel = 
+  //w = vel/radius;
+
   
 
-  
+}
 
   
  
 
-  
-}
+ 
